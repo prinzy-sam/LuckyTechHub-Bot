dc6ab53430daf1f23e87ebb9a50a65ee5e569955
prinzy-sam <prinzysammy@gmail.com>
2025-07-08 08:37:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Lucky Tech Hub Bot',
   author: '‎',
   botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'prince sammy', // Your name
+  ownerNumber: '2347044294617', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
 };
